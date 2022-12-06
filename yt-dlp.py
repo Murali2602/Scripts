a02eac234f11d4cf496c42d2f97aab8cf994c281
Murali <murali@Murali.ht.home>
2022-12-06 13:16:55 -0500

    Added New File

---
+++ yt-dlp.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+
+
+def cli():
+    #Ask for Video URL
+    url = input('Enter URL: ')
+
+    while True:
+        print("1.Show Formats")
+        print("2.Download Format")
+        user_input = int(input())
+        if user_input == 1:
+            formats = subprocess.run(['yt-dlp', '-F', url])
+            pass
+        elif user_input == 2:
+            # Ask where to save the file
+            directory = int(input('Where do u want to save the file?\n1.Songs\n2.Videos\n'))
+            # Change the current working Directory
+            if directory == 1:
+                directory = "/home/murali/Songs"
+            elif directory == 2:
+                directory = "/home/murali/Videos/"
+            audio_code = input('Enter Audio Code: \n')
+            video_code = input('Enter Video Code: \n')
+            download = subprocess.run(['yt-dlp', '-f', audio_code+'+'+video_code, '--embed-thumbnail', url], cwd=directory)
+            if download.returncode == 0:
+                print()
+                prompt = input('Do you want to continue? (yes/no)')
+                if prompt == "yes":
+                    url = input('Enter URL: ')
+                    continue
+                else:
+                    break
+        else:
+            break
+
+
+
+cli()
