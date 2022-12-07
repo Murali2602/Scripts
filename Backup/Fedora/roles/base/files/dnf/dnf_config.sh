aea7f32ff4926474cf383290557500bcc51a5596
Murali <murali@Murali.ht.home>
2022-12-06 20:21:58 -0500

    Made Changes

--- Backup/Fedora/roles/base/files/dnf/dnf_config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-#
-echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
-echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
-echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
