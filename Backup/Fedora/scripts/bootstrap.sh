a5ce11fe15b674f4a5e93d04e9c1103a4e5ed3b9
Murali <murali.ramachandra9@gmail.com>
2022-12-18 02:53:01 -0500

    Made changes

--- Backup/Fedora/scripts/bootstrap.sh
+++ Backup/Fedora/scripts/bootstrap.sh
@@ -10,15 +10,20 @@ sudo dnf install -y git ansible-core
 cd ~/ && git clone https://github.com/Murali2602/Scripts
 
 
-## Import keys -
-#Import both public and private keys
+#### Import keys -
+###Import both public and private keys
+##Check if the keys exist or not 
+FILE=/home/murali/Downloads/*.txt
 #
-gpg --import /home/murali/Downloads/public.txt
-gpg --import /home/murali/Downloads/private.txt
+if [[ ! -f "$FILE" ]]; then 
+	echo "The Private and Public Keys do not exist! Transfer them and then rerun the script"
+else
+
+	gpg --import /home/murali/Downloads/public.txt
+	gpg --import /home/murali/Downloads/private.txt
 
 
-
-### Call the ansible playbook
-cd Scripts/Backup/Fedora/
-ansible-playbook --ask-become-pass site.yml
-
+	### Call the ansible playbook
+	cd Scripts/Backup/Fedora/
+	ansible-playbook --ask-become-pass site.yml
+fi
